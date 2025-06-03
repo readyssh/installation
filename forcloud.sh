37b2fae6da36a072cf07019b36a3ea55d776d887
readyssh <shreyashdeveloper09@gmail.com>
2025-06-03 09:42:36 +0530

    Update forcloud.sh

--- forcloud.sh
+++ forcloud.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
+
 # On your EC2 machine (once)
 sudo apt update
 sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Add Jenkins user to Docker group
 sudo usermod -aG docker jenkins
-
-# Then restart Jenkins
 sudo systemctl restart jenkins
